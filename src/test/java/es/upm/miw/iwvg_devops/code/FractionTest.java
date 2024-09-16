901c24cb2ae1ce6d96ac761f23d6c19f96f3c7bb
rafaelacirino <rafaelaborbaf@gmail.com>
2024-09-16 19:03:47 +0200

    Adding new functionalities in classes Fraction and FractionTest issue#8

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -2,8 +2,7 @@ package es.upm.miw.iwvg_devops.code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FractionTest {
 
@@ -33,6 +32,77 @@ public class FractionTest {
     public void testDecimal(){
         Fraction fraction = new Fraction(1, 2);
         assertEquals(0.5, fraction.decimal());
+    }
+
+    @Test
+    public void testIsProper(){
+        Fraction fraction = new Fraction(3, 4);
+        assertTrue(fraction.isProper(), "Fraction 3/4 should be proper");
+
+        Fraction improperFraction = new Fraction(5, 4);
+        assertFalse(improperFraction.isProper(), "Fraction 5/4 should not be proper");
+    }
+
+    @Test
+    public void testIsImproper(){
+        Fraction fraction = new Fraction(5, 4);
+        assertTrue(fraction.isImproper(), "Fraction 5/4 should be improper");
+
+        Fraction properFraction = new Fraction(3, 7);
+        assertFalse(properFraction.isImproper(), "Fraction 3/7 should not be improper");
+    }
+
+    @Test
+    public void testIsEquivalent(){
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(2, 4);
+
+        assertTrue(fraction1.isEquivalent(fraction2), "Fraction 1/2 and 2/4 should be equivalent");
+
+        Fraction fraction3 = new Fraction(3, 4);
+        assertFalse(fraction1.isEquivalent(fraction3), "Fraction 1/2 and 3/4 should not be equivalent");
+    }
+
+    @Test
+    public void testAdd(){
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(1, 3);
+        Fraction result = fraction1.add(fraction2);
+
+        assertEquals(5, result.getNumerator(), "Numerator of 1/2 + 1/3 should be 5");
+        assertEquals(6, result.getDenominator(), "Denominator of 1/2 + 1/3 should be 6");
+    }
+
+    @Test
+    public void testMultiply(){
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(2, 3);
+        Fraction result = fraction1.multiply(fraction2);
+
+        assertEquals(1, result.getNumerator(), "Numerator of 1/2 * 2/3 should be 1");
+        assertEquals(3, result.getDenominator(), "Denominator of 1/2 * 2/3 should be 3");
+    }
+
+    @Test
+    public void testDivide(){
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(2, 3);
+        Fraction result = fraction1.divide(fraction2);
+
+        assertEquals(3, result.getNumerator(), "Numerator of 1/2 divided by 2/3 should be 3");
+        assertEquals(4, result.getDenominator(), "Denominator of 1/2 divided by 2/3 should be 4");
+    }
+
+    @Test
+    public void testDividedWhenNumeratorIsZero(){
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(0, 3);
+
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            fraction1.divide(fraction2);
+        });
+
+        assertEquals("Cannot divide when numerator is zero", exception.getMessage());
     }
 
     @Test
